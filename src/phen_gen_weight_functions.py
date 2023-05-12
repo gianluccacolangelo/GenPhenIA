6002422456f297812860d0ea010d6ddcdadef818
jeanlucc <gianlucisnt@gmail.com>
2023-05-12 19:17:54 -0300

    Primera corrida de calculos de capitalidad especificidad y similaridad sobre el set simulado mph0.2 e iph0.1, las correlaciones entre los 3 pesos son muy raras. No sé bien si se debe a que el ruido es uniforme, es decir, siempre hay 2 missin phenotypes y 1 incorrect phenotype

--- src/phen_gen_weight_functions.py
+++ src/phen_gen_weight_functions.py
@@ -6,6 +6,8 @@ tanto en general, como para los pacientes en particular.
 ## {{{ IMPORTACIONES
 import numpy as np
 import json
+import pandas as pd
+import matplotlib.pyplot as plt
 PATH = '/home/brainy/Desktop/1ercuatri2023/Tesis/GenPhenIA/'
 ## }}}
 
@@ -98,32 +100,91 @@ la base de datos.
 
 
 
+## {{{
+def lista_de_genes(db=f'{PATH}data/simulated/gene_phenotype_dict.json'):
+    with open(db,'r') as file:
+        gene_phenotype_dict = json.load(file)
+    lista = [i for i in gene_phenotype_dict.keys()]
+    return lista
 
-def pacient_gene_score(pacient_genes_list):
-    """
-Esta función toma la lista de raw_phen_to_genes del paciente y le asigna un score
-a los elementos en base a su capitalidad y su especificidad con respecto a
-pacient_phen_list
-    """
+def corrida_de_pesos():
 
-def pacient_phen_score(pacient_phen_list):
-    """
-Esta función toma la lista de fenotipos y devuelve un diccionario (u otra
-clase) con sus scores.
-    """
+    lista = lista_de_genes()
 
-def general_gene_score(genes_to_phenotype):
-    """
-Esta función toma el archivo genes_to_phenotype.txt y ...
-acá hay que hacer redes y centralidades
-    """
+    df = pd.DataFrame(columns=['gen', 'especificidad', 'capitalidad', 'similaridad'])
+    i=0
+    for gen in lista:
+        fen_reales = fen_reales_del_gen(gen)
+        fen_observados = fen_observados_con_ruido(gen,0.2,0.1)
+        especificidad = especificidad_del_gen(fen_observados,fen_reales)
+        capitalidad = capitalidad_del_gen(fen_observados,fen_reales)
+        similaridad = similaridad_del_gen(fen_observados,fen_reales)
+        """
+        And now we will save the results in a matrix where the columns are
+        gen and the rows are especificidad, capitalidad and similaridad
+        """
+        df = df.append({
+            'gen': gen,
+            'especificidad': especificidad,
+            'capitalidad': capitalidad,
+            'similaridad': similaridad
+        }, ignore_index=True)
+        print(f'Calculando...{i/4992*100:.2f}%', end='\r')
+        i+=1
 
-def general_phen_score(phenotype_to_genes):
-    """
-Esta función toma el archivo phenoype_to_genes.txt y le asigna un score a todos
-los fenotipos según la relación 1/n° genes que lo causan. De modo que aquél
-fenotipo que tiene una relación 1:1 tiene el máximo score.
-    """
+    return df
+
+## }}}
+
+def plot_correlations(df):
+    parameters = ['especificidad', 'capitalidad', 'similaridad']
+    n = len(parameters)
+
+    # Create a 3x3 grid of subplots
+    fig, axs = plt.subplots(n, n, figsize=(15, 15))
+
+    for i in range(n):
+        for j in range(n):
+            # Scatter plot for each pair of parameters
+            axs[i, j].scatter(df[parameters[i]], df[parameters[j]], alpha=0.5)
+
+            # Set the title to the correlation coefficient
+            corr_coef = np.corrcoef(df[parameters[i]], df[parameters[j]])[0, 1]
+            axs[i, j].set_title(f'Correlation: {corr_coef:.2f}')
+
+            # Set the x and y labels
+            axs[i, j].set_xlabel(parameters[i])
+            axs[i, j].set_ylabel(parameters[j])
+
+    plt.tight_layout()
+    plt.show()
+
+
+# def pacient_gene_score(pacient_genes_list):
+    # """
+# Esta función toma la lista de raw_phen_to_genes del paciente y le asigna un score
+# a los elementos en base a su capitalidad y su especificidad con respecto a
+# pacient_phen_list
+    # """
+
+# def pacient_phen_score(pacient_phen_list):
+    # """
+# Esta función toma la lista de fenotipos y devuelve un diccionario (u otra
+# clase) con sus scores.
+    # """
+
+# def general_gene_score(genes_to_phenotype):
+    # """
+# Esta función toma el archivo genes_to_phenotype.txt y ...
+# acá hay que hacer redes y centralidades
+    # """
+
+# def general_phen_score(phenotype_to_genes):
+    # """
+# Esta función toma el archivo phenoype_to_genes.txt y le asigna un score a todos
+# los fenotipos según la relación 1/n° genes que lo causan. De modo que aquél
+# fenotipo que tiene una relación 1:1 tiene el máximo score.
+    # """
 
 
 
