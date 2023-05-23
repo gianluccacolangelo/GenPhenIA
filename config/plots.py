4e3ccc608c61f2700ec5af7154e4fe7586bfb129
jeanlucc <gianlucisnt@gmail.com>
2023-05-23 13:35:49 -0300

    Creo el archivo plots.py para tener todos los plots ahí

---
+++ config/plots.py
@@ -0,0 +1,10 @@
+"""
+En este archivo están los plots de todos los gráficos.
+"""
+
+## {{{ importaciones
+sys.path.insert(0,'/home/brainy/Desktop/1ercuatri2023/Tesis/GenPhenIA/src')
+import phen_gen_weight_functions as pgw
+import linear_model_lab as lml
+
+## }}}
