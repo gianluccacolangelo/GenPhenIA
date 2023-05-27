d1f22ee99711f9da573fd9aa5d40c1a1db7e6f08
jeanlucc <gianlucisnt@gmail.com>
2023-05-27 19:40:04 -0300

    Escribo la función que me da mph e iph de la base de datos real que le demos. Ahora hice contra la base redundante phen to gen, es decir, que tiene desde los fenotipos más generales hasta los más específicos

--- scripts/exploring_real_sets.py
+++ scripts/exploring_real_sets.py
@@ -19,6 +19,8 @@ import ast
 import json
 import scienceplots
 import matplotlib.pyplot as plt
+import pandas as pd
+import csv
 ## }}}
 
 ## {{{ Convertir a diccionario el set que nos dieron
@@ -64,31 +66,72 @@ with open(f'{PATH}data/real/clinvar.json','r') as f:
 bitgenia_total_phenotypes = [len(phen_set) for phen_set in bitgenia.values()]
 clinvar_total_phenotypes = [len(phen_set) for phen_set in clinvar.values()]
 
-with plt.style.context(['science','ieee','nature']):
-    fig, (ax1,ax2) = plt.subplots(1,2)
-    ax1.hist(bitgenia_total_phenotypes,bins=20,alpha=.7)
-    ax1.set_title("Bitgenia")
-    ax1.set_xlabel("Fenotipos observados totales (N)",size=4)
-    ax1.set_ylabel("Frecuencia",size=4)
-    ax1.axvline(x=np.mean(bitgenia_total_phenotypes),linestyle="--",
-            label=f"media $={np.round(np.mean(bitgenia_total_phenotypes))}$")
-    ax1.tick_params(axis='both',which='major',labelsize=3)
-    ax1.legend(fontsize=5)
-    ax1.text(20,60,f"std $= {np.round(np.std(bitgenia_total_phenotypes))}$",fontsize=4)
+# with plt.style.context(['science','ieee','nature']):
+    # fig, (ax1,ax2) = plt.subplots(1,2)
+    # ax1.hist(bitgenia_total_phenotypes,bins=20,alpha=.7)
+    # ax1.set_title("Bitgenia")
+    # ax1.set_xlabel("Fenotipos observados totales (N)",size=4)
+    # ax1.set_ylabel("Frecuencia",size=4)
+    # ax1.axvline(x=np.mean(bitgenia_total_phenotypes),linestyle="--",
+            # label=f"media $={np.round(np.mean(bitgenia_total_phenotypes))}$")
+    # ax1.tick_params(axis='both',which='major',labelsize=3)
+    # ax1.legend(fontsize=5)
+    # ax1.text(20,60,f"std $= {np.round(np.std(bitgenia_total_phenotypes))}$",fontsize=4)
 
-    ax2.hist(clinvar_total_phenotypes,bins=20,alpha=.7)
-    ax2.set_title("Clinvar")
-    ax2.set_xlabel("Fenotipos observados totales (N)",size=4)
-    ax2.axvline(x=np.mean(clinvar_total_phenotypes),linestyle="--",
-            label=f"media $={np.round(np.mean(clinvar_total_phenotypes))}$")
-    ax2.tick_params(axis='both',which='major',labelsize=3)
-    ax2.legend(fontsize=5)
-    ax2.text(25,360,f"std $= {np.round(np.std(clinvar_total_phenotypes))}$",fontsize=4)
+    # ax2.hist(clinvar_total_phenotypes,bins=20,alpha=.7)
+    # ax2.set_title("Clinvar")
+    # ax2.set_xlabel("Fenotipos observados totales (N)",size=4)
+    # ax2.axvline(x=np.mean(clinvar_total_phenotypes),linestyle="--",
+            # label=f"media $={np.round(np.mean(clinvar_total_phenotypes))}$")
+    # ax2.tick_params(axis='both',which='major',labelsize=3)
+    # ax2.legend(fontsize=5)
+    # ax2.text(25,360,f"std $= {np.round(np.std(clinvar_total_phenotypes))}$",fontsize=4)
+## }}}
+
+
+## {{{ Ahora es hora de empezar a ver las distribuciones de mph e iph
+
+"""
+En esta parte vamos a utilizar el diccionario phenotype_gene_dict.json y para
+cada gen, vamos a hacer la resta entre FO de bitgenia o clinvar, y F
+registrados para ese gen.
+"""
+with open (f"{PATH}data/simulated/phenotype_gene_dict.json",'r') as f:
+    phenotype_gene_dict = json.load(f)
+    for phen in phenotype_gene_dict:
+        phenotype_gene_dict[phen] = [int(gene.split(':')[-1])
+                for gene in phenotype_gene_dict[phen]]
+
+def mph(db_curada,db_real):
+    """
+    Esta función calcula la cantidad de mph que hay en una base de datos real
+    """
+    mph = []
+    iph = []
+    for gene in db_real:
+        phens_curados = list(db_curada[db_curada[2] == gene][0].values)
+        phens_curados = [int(phen.split(':')[-1]) for phen in phens_curados]
+        phens_reales = db_real[gene]
+        mph.append((len(phens_curados) -
+                len(set(phens_curados).intersection(set(phens_reales))))/
+                len(set(phens_reales).union(set(phens_curados))))
+        iph.append(((len(phens_reales)) -
+                len(set(phens_curados).intersection(set(phens_reales))))/
+                len(set(phens_reales).union(set(phens_curados))))
+
+    return mph , iph
+
+
+with open(f'{PATH}data/phenotype_to_genes.txt','r') as file:
+    reader = csv.reader(file,delimiter='\t')
+    df = pd.DataFrame(reader)
+
+
 
 ## }}}
 
 
-## {{{
+## {{{ plotting
 
 
 ## }}}
