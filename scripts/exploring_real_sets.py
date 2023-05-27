f7fc8c7a893f198e9b4c83cd9ecff3eaba575375
jeanlucc <gianlucisnt@gmail.com>
2023-05-27 12:26:30 -0300

    escribo la función para convertir el .sets a un diccionario y luego lo guardo en un .json

--- scripts/exploring_real_sets.py
+++ scripts/exploring_real_sets.py
@@ -16,8 +16,36 @@ sys.path.insert(0,'/home/brainy/Desktop/1ercuatri2023/Tesis/GenPhenIA/src')
 import linear_model_lab as lml
 import phen_gen_weight_functions as pgw
 PATH = "/home/brainy/Desktop/1ercuatri2023/Tesis/GenPhenIA/"
+import ast
+import json
+
 ## }}}
 
-## {{{ La
+## {{{ Convertir a diccionario el set que nos dieron
+
+def convert_to_dict(file_path):
+    with open(file_path, 'r') as f:
+        content = f.read()
+
+    # This will give us a list of tuples
+    list_of_tuples = ast.literal_eval(content)
+
+    # Now, we will convert it to a dictionary
+    # We'll also convert the string representation of list to an actual list using ast.literal_eval
+    dictionary = {t[0]: ast.literal_eval(t[1]) for t in list_of_tuples}
+
+    return dictionary
+
+file_path = f"{PATH}data/real/bitgenia_IDs_tov3.sets"
+dictionary = convert_to_dict(file_path)
+# print(dictionary)
+
+with open(f'{PATH}data/real/bitgenia.json', 'w') as f:
+    json.dump(dictionary, f)
+
+## }}}
+
+
+## {{{
 
 ## }}}
